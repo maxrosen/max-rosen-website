d193b74c295902b2f5ae3f0d8347f276977b2599
Max Rosen <mfrosen100@gmail.com>
2019-05-13 12:23:26 -0400

    Updates

---
+++ precache-manifest.7132b037f15aed2d3930db9e3fe70402.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16ed647bc5b39d92393de4bb566510c0",
+    "url": "/maxrosen/max-rosen-website/index.html"
+  },
+  {
+    "revision": "bd06f1488d328fc26ebd",
+    "url": "/maxrosen/max-rosen-website/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "f00e170518af02ab73e0",
+    "url": "/maxrosen/max-rosen-website/static/js/2.b288facb.chunk.js"
+  },
+  {
+    "revision": "bd06f1488d328fc26ebd",
+    "url": "/maxrosen/max-rosen-website/static/js/main.b5b47dee.chunk.js"
+  },
+  {
+    "revision": "79a14b6490ded2ad3645",
+    "url": "/maxrosen/max-rosen-website/static/js/runtime~main.7d5335ad.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/maxrosen/max-rosen-website/static/media/logo.5d5d9eef.svg"
+  }
+]);
